d5ad55ebb58331170de8917e4d1686803ae7ae63
Tiyani <tiyani@geekulcha.com>
2020-02-19 16:38:29 +0200

    Updates

---
+++ precache-manifest.d9ebf7240523dd44fec988a62774ab0a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a55728ae4180a45bb5ba2fdf1c017b",
+    "url": "/gk1styear/index.html"
+  },
+  {
+    "revision": "b980824226f8f9db422a",
+    "url": "/gk1styear/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b980824226f8f9db422a",
+    "url": "/gk1styear/static/js/2.67d6e50a.chunk.js"
+  },
+  {
+    "revision": "ff88717cb533998c860f125fee51fd0e",
+    "url": "/gk1styear/static/js/2.67d6e50a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9cee042f8dfce722877",
+    "url": "/gk1styear/static/js/main.6ef119a3.chunk.js"
+  },
+  {
+    "revision": "32f087dba79bf611660069a68c00c0b9",
+    "url": "/gk1styear/static/js/main.6ef119a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d500f770bde11948a75",
+    "url": "/gk1styear/static/js/runtime-main.7f1810a7.js"
+  },
+  {
+    "revision": "8ebc423a31d3426ca5ede7b243f2ae63",
+    "url": "/gk1styear/static/media/Mpho-Jan-Kubeka.8ebc423a.jpg"
+  },
+  {
+    "revision": "9fd05e5e980866530de45838664535f9",
+    "url": "/gk1styear/static/media/charmaine.9fd05e5e.jpg"
+  },
+  {
+    "revision": "18c3949978d5c159367273b7bc60d15d",
+    "url": "/gk1styear/static/media/hackath.18c39499.jpg"
+  },
+  {
+    "revision": "4c3b573a32f457ae80f24b5c8029b6fa",
+    "url": "/gk1styear/static/media/landing-bg.4c3b573a.jpg"
+  },
+  {
+    "revision": "bf007b3018e0b468d7728881142ac296",
+    "url": "/gk1styear/static/media/ofentswe.bf007b30.jpg"
+  },
+  {
+    "revision": "cdff6f58725ae5e52856effe1911de47",
+    "url": "/gk1styear/static/media/opencampus.cdff6f58.jpg"
+  },
+  {
+    "revision": "97cb7be6459efcf700bc657fca3bc972",
+    "url": "/gk1styear/static/media/opendata.97cb7be6.jpg"
+  },
+  {
+    "revision": "9c1ea00c41e130a644ea29f2e8c7d463",
+    "url": "/gk1styear/static/media/safehack.9c1ea00c.jpg"
+  },
+  {
+    "revision": "c25b6c2b70ca89d446f89214c80213dd",
+    "url": "/gk1styear/static/media/sewagodimo.c25b6c2b.jpg"
+  },
+  {
+    "revision": "b0e64b5c5d0f5db040e9084a235dc191",
+    "url": "/gk1styear/static/media/sifiso.b0e64b5c.jpg"
+  },
+  {
+    "revision": "909a9dc878bac8c0f343cc642a86dbea",
+    "url": "/gk1styear/static/media/skatehacks.909a9dc8.jpg"
+  },
+  {
+    "revision": "eed568c5f73bea68bc987f09b22b370a",
+    "url": "/gk1styear/static/media/tshileretso.eed568c5.jpg"
+  },
+  {
+    "revision": "2d58d23b942d8a3d764cb1bbf3df1c97",
+    "url": "/gk1styear/static/media/women.2d58d23b.jpg"
+  }
+]);

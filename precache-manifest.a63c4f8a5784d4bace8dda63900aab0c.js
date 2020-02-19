824f875c5f6eba21821729e043e373f597a75059
Tiyani <tiyani@geekulcha.com>
2020-02-19 16:12:41 +0200

    Updates

---
+++ precache-manifest.a63c4f8a5784d4bace8dda63900aab0c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13ac4f04ca77e446bc097f4b4511a2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b980824226f8f9db422a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b980824226f8f9db422a",
+    "url": "/static/js/2.67d6e50a.chunk.js"
+  },
+  {
+    "revision": "ff88717cb533998c860f125fee51fd0e",
+    "url": "/static/js/2.67d6e50a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f3b116e90c0851252a4",
+    "url": "/static/js/main.0b13fe31.chunk.js"
+  },
+  {
+    "revision": "32f087dba79bf611660069a68c00c0b9",
+    "url": "/static/js/main.0b13fe31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed94408b5f0a84119e3a",
+    "url": "/static/js/runtime-main.2a9725e7.js"
+  },
+  {
+    "revision": "8ebc423a31d3426ca5ede7b243f2ae63",
+    "url": "/static/media/Mpho-Jan-Kubeka.8ebc423a.jpg"
+  },
+  {
+    "revision": "9fd05e5e980866530de45838664535f9",
+    "url": "/static/media/charmaine.9fd05e5e.jpg"
+  },
+  {
+    "revision": "18c3949978d5c159367273b7bc60d15d",
+    "url": "/static/media/hackath.18c39499.jpg"
+  },
+  {
+    "revision": "4c3b573a32f457ae80f24b5c8029b6fa",
+    "url": "/static/media/landing-bg.4c3b573a.jpg"
+  },
+  {
+    "revision": "bf007b3018e0b468d7728881142ac296",
+    "url": "/static/media/ofentswe.bf007b30.jpg"
+  },
+  {
+    "revision": "cdff6f58725ae5e52856effe1911de47",
+    "url": "/static/media/opencampus.cdff6f58.jpg"
+  },
+  {
+    "revision": "97cb7be6459efcf700bc657fca3bc972",
+    "url": "/static/media/opendata.97cb7be6.jpg"
+  },
+  {
+    "revision": "9c1ea00c41e130a644ea29f2e8c7d463",
+    "url": "/static/media/safehack.9c1ea00c.jpg"
+  },
+  {
+    "revision": "c25b6c2b70ca89d446f89214c80213dd",
+    "url": "/static/media/sewagodimo.c25b6c2b.jpg"
+  },
+  {
+    "revision": "b0e64b5c5d0f5db040e9084a235dc191",
+    "url": "/static/media/sifiso.b0e64b5c.jpg"
+  },
+  {
+    "revision": "909a9dc878bac8c0f343cc642a86dbea",
+    "url": "/static/media/skatehacks.909a9dc8.jpg"
+  },
+  {
+    "revision": "eed568c5f73bea68bc987f09b22b370a",
+    "url": "/static/media/tshileretso.eed568c5.jpg"
+  },
+  {
+    "revision": "2d58d23b942d8a3d764cb1bbf3df1c97",
+    "url": "/static/media/women.2d58d23b.jpg"
+  }
+]);

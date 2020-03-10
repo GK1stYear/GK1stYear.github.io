950b518fd3ec586527120dda50a6381a6d7e99f1
Carol Khoza <Your-Email>
2020-03-10 12:10:35 +0200

    update

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -46,35 +46,29 @@ export default function LandingPage(props) {
   const classes = useStyles();
   const { ...rest } = props;
   const data = {
-    title: "FAQ (How it works)",
+    title: "FAQ",
     rows: [
       {
-        title: "Lorem ipsum dolor sit amet,",
-        content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat, 
-                ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus. 
-                In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae. 
-                Fusce sed commodo purus, at tempus turpis.`
+        title: "Why should i be part of this programme?",
+        content: `If you're looking for a way to kick-start your career and if you want to be exposed to technology, this would be a great opportunity for you!`
       },
       {
-        title: "Nunc maximus, magna at ultricies elementum",
-        content: "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor."
+        title: "What will I gain?",
+        content: "You'll gain more knowledge, get exposure, and take part of tech events specially tailored for first years."
       },
       {
-        title: "Curabitur laoreet, mauris vel blandit fringilla",
-        content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem. 
-              Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam. 
-              Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat. 
-              Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `
+        title: "How do i join?",
+        content: "............."
       },
       {
-        title: "What is the package version",
-        content: "v1.0.0"
+        title: "What's next?",
+        content: ".........."
       }]
   }
   const styles = {
     // bgColor: 'white',
-    titleTextColor: 'blue',
-    rowTitleColor: 'blue',
+    titleTextColor: 'black',
+    rowTitleColor: 'black',
     // rowContentColor: 'grey'
   }
   
